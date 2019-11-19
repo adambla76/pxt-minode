cf8d832b38400730cf9e26a38e2835a4db9736ab
adambla76 <adambla76@gmail.com>
2019-11-19 15:17:04 +0100

    Updates.

--- tests.ts
+++ tests.ts
@@ -7,6 +7,5 @@ input.onButtonPressed(Button.A, () => {
     minode.FanControl_1(AnalogConnName.Analog_A2, 80)
 })
 basic.forever(() => {
-    basic.showNumber(minode.DHTGetTemperature(ConnName.D13, DHTTemStyle.MINODE_DHT_CELSIUS))
     basic.pause(100)
 })
